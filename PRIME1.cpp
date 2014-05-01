8b9895d2c4fab114db8a51a2fe49353509ac8946
Anmol <garganmol1993@gmail.com>
2014-05-02 02:29:11 +0530

    Finally eratosthenes done

--- PRIME1.cpp
+++ PRIME1.cpp
@@ -1,31 +1,52 @@
+//Sieve of Eratosthenes
+
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <cstdlib>
+
+
 using namespace std;
 
-
-
 int main () {
+
+	vector <int> primes;
+	primes.push_back(2);
+
+	for (int i = 3; i <= 32118; i++) {
+		int val = sqrt(i) + 1;
+		bool is_prime = true;
+		for (int j = 0; j < primes.size(); j++) {
+			if (primes[j] > val) break;
+			if (i%primes[j] == 0) {
+				is_prime = false;
+				break;
+			}
+		}
+		if (is_prime) primes.push_back(i);	
+	}
+
 	int t;
 	cin>>t;
 	while (t--) {
+
 		int m, n;
 		cin>>m>>n;
-		vector<bool> vec(n+1,1);
-		vec[1] = 0;
-
-		for (int i = 2 ; i <=sqrt(n); i++) {
-			if (vec[i]) {
-				for (int j = i*i; j <=n; j+=i) {
-					vec[j] = 0;
+		
+		for (int i = m ; i <= n; i++) {
+			if (i==1) continue;
+			int k = sqrt(i);
+			bool pr = true;
+			for (int j = 0; primes[j] <= k; j++) {
+				if (i%primes[j] == 0){
+					pr = false;
+					break;
 				}
 			}
-		}
-
-		for (int i = m; i <=n; i++) {
-			if (vec[i]) cout << i << endl;
+			if (pr) cout << i << endl;
 		}
 
 		cout << endl;	
 	}
+
 }
