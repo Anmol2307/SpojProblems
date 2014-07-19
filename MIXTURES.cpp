fa0169e59b76a519e1e995b28ccf20520449c212
Anmol <garganmol1993@gmail.com>
2014-07-19 23:11:26 +0530

    MIXTURES done

--- MIXTURES.cpp
+++ MIXTURES.cpp
@@ -66,3 +66,39 @@ int stringtoint(string A)
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 
+int main () {
+
+  int n;
+  while (scanf("%d",&n) == 1) {
+    int arr[n];
+    for (int i=0; i < n; i++) {
+      scanf("%d",&arr[i]);
+    }
+    int dp[n][n];
+    int cdp[n][n];
+
+    for (int i=0; i<n; i++) {
+      dp[i][i]=arr[i];
+      cdp[i][i]=0;
+    }
+
+    for (int l =2; l <= n; l++) {
+      // printf("%s\n","HERE");
+      for (int i = 0; i < n-l+1; i++) {
+        int j = i+l-1;
+        cdp[i][j] = INT_MAX;
+        for (int k =i; k <= j-1; k++) {
+          int cost = dp[i][k]*dp[k+1][j];
+          // printf("%d %d %d\n",i,k,j);
+          if ((cost + cdp[k+1][j] + cdp[i][k]) < cdp[i][j]) {
+            dp[i][j] = (dp[i][k] + dp[k+1][j])%100;
+            cdp[i][j] = cost + cdp[k+1][j] + cdp[i][k];
+            // printf("HERE %d\n",cdp[i][j]);
+          }
+        }
+      }
+    }
+    printf("%d\n",cdp[0][n-1]);
+  }
+
+}
