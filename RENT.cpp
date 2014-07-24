bc28ef6ee0ec0bba40344c5a1228bf96d088bb61
Anmol <garganmol1993@gmail.com>
2014-07-24 19:33:10 +0530

    Updating code

---
+++ RENT.cpp
@@ -0,0 +1,156 @@
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <sstream>
+#include <algorithm>
+#include <cstdlib>
+#include <cstring>
+#include <map>
+#include <set>
+#include <queue>
+#include <cctype>
+#include <list>
+#include <stack>
+#include <fstream>
+#include <utility>
+#include <iomanip>
+#include <climits>
+
+using namespace std;
+#define pb push_back
+#define all(s) s.begin(),s.end()
+#define f(i,a,b) for(int i=a;i<b;i++)
+#define F(i,a,b) for(int i=a;i>=b;i--)
+#define PI 3.1415926535897932384626433832795
+#define BIG_INF 7000000000000000000LL
+#define mp make_pair
+#define eps 1e-9
+#define si(n) scanf("%d",&n)
+#define sll(n) scanf("%lld",&n)
+#define mod 1000000007
+#define mm 10000000
+#define INF (1<<29)
+#define SET(a) memset(a,-1,sizeof(a))
+#define CLR(a) memset(a,0,sizeof(a))
+#define FILL(a,v) memset(a,v,sizeof(a))
+#define EPS 1e-9
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+#define READ freopen("input.txt", "r", stdin)
+#define WRITE freopen("output.txt", "w", stdout)
+
+
+typedef long long int LL;
+
+string inttostring(int n)
+{
+  stringstream a;
+  a<<n;
+  string A;
+  a>>A;
+  return A;
+}
+
+int stringtoint(string A)
+{
+  stringstream a;
+  a<<A;
+  int p;
+  a>>p;
+  return p;
+}
+
+inline void inp(int &n ) {//fast input function
+    n=0;
+    int ch=getchar(),sign=1;
+    while( ch < '0' || ch > '9' ){if(ch=='-')sign=-1; ch=getchar();}
+    while( ch >= '0' && ch <= '9' )
+        n=(n<<3)+(n<<1)+ ch-'0', ch=getchar();
+    n=n*sign;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+int main () {
+  int t;
+  inp(t);
+
+  while (t--) {
+    int n;
+    inp(n);
+
+    int st[n],dur[n],pr[n];
+
+    for (int i = 0; i < n; i++) {
+      inp(st[i]); inp(dur[i]); inp(pr[i]);
+    }
+
+    LL dp[n];
+    dp[0]=pr[0];
+    for (int i=1; i < n; i++) {
+      dp[i] = -1;
+      for (int j=0; j < i; j++) {
+        if (st[j] + dur[j] <= st[i]) dp[i] = max(dp[i],dp[j]+pr[i]); 
+      }
+    }
+
+    LL max = -1;
+    for (int i =0 ; i < n; i++) {
+      if (max < dp[i]) max = dp[i];
+    }
+    printf("%lli\n",max);
+
+  }
+}
+
+int CeilIndex(int A[], int l, int r, int key) {
+    int m;
+ 
+    while( r - l > 1 ) {
+        m = l + (r - l)/2;
+        (A[m] >= key ? r : l) = m; // ternary expression returns an l-value
+    }
+ 
+    return r;
+}
+ 
+int LongestIncreasingSubsequenceLength(int A[], int size) {
+    // Add boundary case, when array size is one
+ 
+    int *tailTable   = new int[size];
+    int len; // always points empty slot
+ 
+    memset(tailTable, 0, sizeof(tailTable[0])*size);
+ 
+    tailTable[0] = A[0];
+    len = 1;
+    for( int i = 1; i < size; i++ ) {
+        if( A[i] < tailTable[0] )
+            // new smallest value
+            tailTable[0] = A[i];
+        else if( A[i] > tailTable[len-1] )
+            // A[i] wants to extend largest subsequence
+            tailTable[len++] = A[i];
+        else
+            // A[i] wants to be current end candidate of an existing subsequence
+            // It will replace ceil value in tailTable
+            tailTable[CeilIndex(tailTable, -1, len-1, A[i])] = A[i];
+    }
+ 
+    delete[] tailTable;
+ 
+    return len;
+}
+ 
+int main() {
+    int A[] = { 2, 5, 3, 7, 11, 8, 10, 13, 6 };
+    int n = ARRAY_SIZE(A);
+ 
+    printf("Length of Longest Increasing Subsequence is %d\n",
+            LongestIncreasingSubsequenceLength(A, n));
+ 
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,53 +1,26 @@
-#include <vector>
-#define REP(i,n) for (int i = 1; i <= n; i++)
-using namespace std;
+void factor(int n) 
+     { 
+       int i; 
+       for(i=2;i<=(int)sqrt(n);i++) 
+       { 
+         while(n % i == 0) 
+         { 
+           printf("%d ",i); 
+           n /= i; 
+         } 
+       } 
+       if (n > 1) printf("%d",n); 
+       printf("\n"); 
+     }  
 
-typedef long long ll;
-typedef vector<vector<ll> > matrix;
-const ll MOD = 1000000007;
-const int K = 2;
-
-// computes A * B
-matrix mul(matrix A, matrix B)
-{
-    matrix C(K+1, vector<ll>(K+1));
-    REP(i, K) REP(j, K) REP(k, K)
-        C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % MOD;
-    return C;
-}
-
-// computes A ^ p
-matrix pow(matrix A, int p)
-{
-    if (p == 1)
-        return A;
-    if (p % 2)
-        return mul(A, pow(A, p-1));
-    matrix X = pow(A, p/2);
-    return mul(X, X);
-}
-
-// returns the N-th term of Fibonacci sequence
-int fib(int N)
-{
-    // create vector F1
-    vector<ll> F1(K+1);
-    F1[1] = 1;
-    F1[2] = 1;
-
-    // create matrix T
-    matrix T(K+1, vector<ll>(K+1));
-    T[1][1] = 0, T[1][2] = 1;
-    T[2][1] = 1, T[2][2] = 1;
-
-    // raise T to the (N-1)th power
-    if (N == 1)
-        return 1;
-    T = pow(T, N-1);
-
-    // the answer is the first row of T . F1
-    ll res = 0;
-    REP(i, K)
-        res = (res + T[1][i] * F1[i]) % MOD;
-    return res;
-}
+         int fi(int n) 
+     { 
+       int result = n; 
+       for(int i=2;i*i <= n;i++) 
+       { 
+         if (n % i == 0) result -= result / i; 
+         while (n % i == 0) n /= i; 
+       } 
+       if (n > 1) result -= result / n; 
+       return result; 
+     } 
