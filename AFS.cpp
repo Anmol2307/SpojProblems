f24fba673d1a711be02b4ce347ff8a0759687bf7
Anmol <garganmol1993@gmail.com>
2014-07-25 22:32:47 +0530

    Tried a few questions

--- AFS.cpp
+++ AFS.cpp
@@ -1,149 +1,164 @@
-/******************************************************************
-***   Problem No    : Spoj 13753                                ***
-***   Problem Name  : Amazing Prime Sequence                    ***
-***   Type          : Number theory , prime                     ***
-***   Author        : Shipu Ahamed (Psycho Timekiller)          ***
-***   E-mail        : shipuahamed01@gmail.com                   ***
-***   University    : BUBT,Dept. of CSE                         ***
-***   Team          : BUBT_Psycho                               ***
-***   Facebook      : http://www.facebook.com/DeesheharaShipu   ***
-******************************************************************/
-#include <bits/stdc++.h>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <sstream>
+#include <algorithm>
+#include <cstdlib>
+#include <cstring>
+#include <map>
+#include <set>
+#include <queue>
+#include <cctype>
+#include <list>
+#include <stack>
+#include <fstream>
+#include <utility>
+#include <iomanip>
+#include <climits>
+
 using namespace std;
- 
-#define ll long long
-#define pi 2*acos(0.0)
-#define all(v) v.begin(),v.end()
- 
-//For Define
-#define for0(i,n) for(__typeof(n) i=0;i<(n);i++)
-#define for1(i,n) for(__typeof(n) i=1;i<=(n);i++)
-#define rfor0(i,n) for(__typeof(n) i=(n);i>=0;i--)
-#define rfor1(i,n) for(__typeof(n) i=(n);i>=1;i--)
-#define For(i,a,b) for(__typeof(b) i=a;i<=b;i++)
- 
-//input
-#define si(t) scanf("%d",&t)
-#define sl(t) scanf("%lld",&t)
-#define sf(t) scanf("%f",&t)
-#define sdb(t) scanf("%lf",&t)
-#define schar(c) scanf("%c",&c)
-#define sstring(s) scanf("%s",s)
-#define ssi(a,b) scanf("%d%d",&a,&b)
-#define ssl(a,b) scanf("%lld%lld",&a,&b)
- 
-//Output
-#define P(a) printf("%d\n",a)
-#define PL(a) printf("%lld\n",a)
-#define PF(a) printf("%f\n",a)
-#define PDB(a) printf("%lf\n",a)
-#define PN(a) printf("%d ",a)
-#define PLN(a) printf("%lld ",a)
-#define PFN(a) printf("%f ",a)
-#define PDBN(a) printf("%lf ",a)
- 
-#define CP(a) cout<<a<<endl
-#define CPN(a) cout<<a<<" "
- 
-//Test Case & New line
-#define Case(no) printf("Case %d: ",++no)
-#define nl puts("")
- 
-//Debug
-#define db(x) cout << #x << " -> " << (x) << endl;
-#define db_sarr(i,a) cout<<#a<<"["<<i<<"] -> "<<a[i]<<" "<<endl;
-#define db_arr(a,start,end) for(ll i=start;i<=end;i++) cout<<#a<<"["<<i<<"] -> "<<a[i]<<" "<<endl;
-#define db_mat(mat,row,col) for(ll i=0;i<row;i++) {for(ll j=0;j<col;j++) cout<<mat[i][j]<<" ";cerr<<endl;}
-#define db_st(a,b,start,end) for(ll i=start;i<=end;i++) cout<<#a<<"["<<i<<"]."<<#b<<" -> "<<a[i].b<<" "<<endl;
- 
-#define ff first
-#define se second
 #define pb push_back
-#define ppb pop_back
-#define mkp(a,b) make_pair(a,b)
-#define ST(v) sort(all(v))
-#define sz(x) (int)x.size()
-#define gcd(a,b) __gcd(a,b)
-#define lcm(a,b) (a*(b/gcd(a,b)))
-#define max3(a,b,c) max(a,max(b,c))
+#define all(s) s.begin(),s.end()
+#define f(i,a,b) for(int i=a;i<b;i++)
+#define F(i,a,b) for(int i=a;i>=b;i--)
+#define PI 3.1415926535897932384626433832795
+#define BIG_INF 7000000000000000000LL
+#define mp make_pair
+#define eps 1e-9
+#define si(n) scanf("%d",&n)
+#define sll(n) scanf("%lld",&n)
+#define mod 1000000007
+#define mm 10000000
+#define INF (1<<29)
+#define SET(a) memset(a,-1,sizeof(a))
+#define CLR(a) memset(a,0,sizeof(a))
+#define FILL(a,v) memset(a,v,sizeof(a))
+#define EPS 1e-9
 #define min3(a,b,c) min(a,min(b,c))
-#define maxall(v) *max_element(all(v))
-#define minall(v) *min_element(all(v))
-#define cover(a,d) memset(a,d,sizeof(a))
-#define popcount(i) __builtin_popcount(i)                         //count one
-#define parity(i)   __builtin_parity(i)       //evenparity 0 and odd parity 1
-#define input freopen("in.txt","r",stdin)
-#define output freopen("out.txt","w",stdout)
-#define un(v) ST(v), (v).erase(unique(all(v)),v.end())
-#define common(a,b) ST(a), ST(b), a.erase(set_intersection(all(a),all(b),a.begin()),a.end())
-#define uncommon(a,b) ST(a), ST(b), a.erase(set_symmetric_difference(all(a),all(b),a.begin()),a.end())
+#define max3(a,b,c) max(a,max(b,c))
+#define READ freopen("input.txt", "r", stdin)
+#define WRITE freopen("output.txt", "w", stdout)
+
+
+typedef long long int LL;
+
+string inttostring(int n)
+{
+  stringstream a;
+  a<<n;
+  string A;
+  a>>A;
+  return A;
+}
+
+int stringtoint(string A)
+{
+  stringstream a;
+  a<<A;
+  int p;
+  a>>p;
+  return p;
+}
+
+inline void inp(int &n ) {//fast input function
+    n=0;
+    int ch=getchar(),sign=1;
+    while( ch < '0' || ch > '9' ){if(ch=='-')sign=-1; ch=getchar();}
+    while( ch >= '0' && ch <= '9' )
+        n=(n<<3)+(n<<1)+ ch-'0', ch=getchar();
+    n=n*sign;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+#define ll unsigned long long
+#define MAX 1000
+#define LMT 35
+#define NUM 1000000
  
-typedef  vector<int> vi;
-typedef  vector<ll> vl;
-typedef  vector<string> vs;
-typedef  pair<int,int> pii;
-typedef  vector<pii> vpii;
-typedef  set<int> si;
-typedef  set<string> ss;
-typedef  map<int,int> mii;
-typedef  map<string,int> msi;
+unsigned flag[MAX/64];
+unsigned prime[MAX], total;
+long long int arr[NUM];
  
-////============ CONSTANT ===============////
-#define MAXLL 9223372036854775807
-#define MINLL 9223372036854775808
-#define MAXL  2147483647
-#define MINL  2147483648
-#define mx7   1000007
-#define mx6   1000006
-#define inf   1<<30                                           //infinity value
-#define eps   1e-9
-#define mx    (100010)
-#define mod   1000000007
-////=====================================////
+#define chkC(n) (flag[n>>6]&(1<<((n>>1)&31)))
+#define setC(n) (flag[n>>6]|=(1<<((n>>1)&31)))
  
-ll pd[mx7+10],a[mx7+10];
-bool prime[mx7+10];
  
-void sieve(){
-        for (ll i = 2; i <=mx7; i+= 2)
-           prime[i]=1, pd[i] = 2;
  
-        for (ll i = 3; i <=mx7; i += 2){
-                if (!prime[i]){
-                    pd[i] = i;
-                    for (ll j=i; (j*i) <=mx7; j += 2)
-                    {
-                        prime[j*i] = 1;
-                        if(!pd[j*i])
-                        pd[j*i] = i;
-                    }
+void sieve()
+{
+        unsigned i, j, k;
+        for(i=3;i<LMT;i+=2)
+                if(!chkC(i))
+                        for(j=i*i,k=i<<1;j<MAX;j+=k)
+                                setC(j);
+        prime[1] = 2;
+     
+        for(i=3,j=2;i<MAX;i+=2)
+        {
+                if(!chkC(i))
+                {
+                    prime[j++] = i;
                 }
+        }                
+        total = j;
+        
+}
+
+void func()
+{
+    unsigned int v=0;
+    arr[0] = 0;
+    arr[1] = 0;
+    for(int i=2;i<NUM;i++)
+    {
+        // int i = a;
+        int h=i;
+        long long int ans=1;
+        for(int j=1,t=prime[j];j<total && t <= sqrt(h); t = prime[++j])
+        {
+                v=0;
+                if(h%t==0)
+                {
+                        
+                        while(h%t==0)
+                        {
+                                v++;
+                                h/=t;
+                        }
+                        
+                }
+                ans=(LL)(((LL)ans)*((LL)(v+1)));
+                if(h==1)
+                    break;
         }
+        if (h != 1) {
+          ans = (LL)(ans*2);
+        }
+        arr[i] = arr[i-1] + ans -1; 
+     }
 }
- 
-void solve()
-{
-    a[0]=0;
-    a[1]=0;
-    for(int i=2;i<=mx7;i++)
-    {
-        a[i]=a[i-1]+pd[i];
-    }
-}
- 
- 
-int main()
-{
+
+
+
+
+
+
+int main  () {
     sieve();
-    solve();
+    func();
     int t;
-    si(t);
-    while(t--)
-    {
+    inp(t);
+
+    while (t--) {
         int n;
-        si(n);
-        PL(a[n]);
- 
+        inp(n);
+        printf("%lli\n",arr[n]);
+        // printf("%i\n",func(n));
     }
-    return 0;
+    
 }
--- GCJ1C09A.cpp
+++ GCJ1C09A.cpp
@@ -1,162 +1,201 @@
-#include <cmath>
-#include <ctime>
+// #include <cmath>
+// #include <ctime>
+// #include <iostream>
+// #include <string>
+// #include <vector>
+// #include <cstdio>
+// #include <sstream>
+// #include <algorithm>
+// #include <cstdlib>
+// #include <cstring>
+// #include <map>
+// #include <set>
+// #include <queue>
+// #include <cctype>
+// #include <list>
+// #include <stack>
+// #include <fstream>
+// #include <utility>
+// #include <iomanip>
+// #include <climits>
+
+// using namespace std;
+// #define pb push_back
+// #define all(s) s.begin(),s.end()
+// #define f(i,a,b) for(int i=a;i<b;i++)
+// #define F(i,a,b) for(int i=a;i>=b;i--)
+// #define PI 3.1415926535897932384626433832795
+// #define BIG_INF 7000000000000000000LL
+// #define mp make_pair
+// #define eps 1e-9
+// #define si(n) scanf("%d",&n)
+// #define sll(n) scanf("%lld",&n)
+// #define mod 1000000007
+// #define mm 10000000
+// #define INF (1<<29)
+// #define SET(a) memset(a,-1,sizeof(a))
+// #define CLR(a) memset(a,0,sizeof(a))
+// #define FILL(a,v) memset(a,v,sizeof(a))
+// #define EPS 1e-9
+// #define min3(a,b,c) min(a,min(b,c))
+// #define max3(a,b,c) max(a,max(b,c))
+// #define READ freopen("input.txt", "r", stdin)
+// #define WRITE freopen("output.txt", "w", stdout)
+
+
+// typedef long long int LL;
+
+// string inttostring(int n)
+// {
+//   stringstream a;
+//   a<<n;
+//   string A;
+//   a>>A;
+//   return A;
+// }
+
+// int stringtoint(string A)
+// {
+//   stringstream a;
+//   a<<A;
+//   int p;
+//   a>>p;
+//   return p;
+// }
+
+// inline void inp(int &n ) {//fast input function
+//     n=0;
+//     int ch=getchar(),sign=1;
+//     while( ch < '0' || ch > '9' ){if(ch=='-')sign=-1; ch=getchar();}
+//     while( ch >= '0' && ch <= '9' )
+//         n=(n<<3)+(n<<1)+ ch-'0', ch=getchar();
+//     n=n*sign;
+// }
+
+// ///////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// int main () {
+
+//   int t;
+//   inp(t);
+//   char arr[63];
+//   int val[37];
+//   memset(val,-1,sizeof(val));
+//   int c = 1;
+//   while (t--) {
+//     scanf("%s",arr);
+//     int len = strlen(arr);
+//     bool used = false;
+    
+//     if (arr[0] >= 97 && val[arr[0]-'a'+10] == -1) {
+//       val[arr[0]-'a'+10] = 1;
+//     }
+//     else if (arr[0] >= 48 && val[arr[0]-'0'] == -1) {
+//       // printf("HERE\n");
+//       val[arr[0]-'0'] = 1;
+//     }
+
+//     if (arr[1] >= 97 && val[arr[1]-'a'+10] == -1) {
+//       val[arr[1]-'a'+10] = 0;
+//       used = true;
+//     }
+//     else if (arr[1] >= 48 && val[arr[1]-'0'] == -1) {
+//       val[arr[1]-'0'] = 0;
+//       used = true;
+//     }
+
+    
+//     int j = 2;
+//     for (int i = 2; i < len; i++) {
+//       if (arr[i] >= 97 && val[arr[i]-'a'+10] == -1) {
+//         if (used) {
+//           val[arr[i]-'a'+10] = j;
+//           j++;
+//         }
+//         else {
+//           val[arr[i]-'a'+10] = 0;
+//           used = true;
+//         }
+//       }
+//       else if (arr[i] >= 48 && val[arr[i]-'0'] == -1) {
+//         if (used) {
+//           val[arr[i]-'0'] = j;
+//           j++;
+//         }
+//         else {
+//           val[arr[i]-'0'] = 0;
+//           used = true;
+//         }
+//       }
+//     }
+
+//     // for (int i = 0; i < len; i++) {
+//     //   if (arr[i] >= 97) {
+//     //     // ans += val[arr[i]-'a'+10]*pow(len,len-i-1);
+//     //     printf("%d ",val[arr[i]-'a'+10]);
+//     //   }
+//     //   else if (arr[i] >= 48) {
+//     //     // ans += val[arr[i]-'0']*pow(len,len-i-1);
+//     //     printf("%d ",val[arr[i]-'0']);
+//     //   }
+//     // }
+//     // printf("\n%d\n", j);
+//     // exit(0);
+
+
+//     long long int ans = 0;
+//     for (int i = 0; i < len; i++) {
+//       if (arr[i] >= 97) {
+//         // ans += (LL)(((LL)val[arr[i]-'a'+10])*((LL)pow(j,len-i-1)));
+//         ans = ans * j + val[arr[i]-'a'+10];
+//       }
+//       else if (arr[i] >= 48) {
+//         // ans += (LL)(((LL)val[arr[i]-'0'])*((LL)pow(j,len-i-1)));
+//         ans = ans * j + val[arr[i]-'0'];
+//       }
+//     }
+//     printf("Case #%d: %lli\n",c,ans);
+//     c++;
+//   }
+// }
+
 #include <iostream>
-#include <string>
 #include <vector>
-#include <cstdio>
+#include <string>
 #include <sstream>
 #include <algorithm>
-#include <cstdlib>
-#include <cstring>
-#include <map>
-#include <set>
-#include <queue>
-#include <cctype>
-#include <list>
 #include <stack>
-#include <fstream>
-#include <utility>
-#include <iomanip>
-#include <climits>
+#include <queue>
+#include <map>
 
 using namespace std;
-#define pb push_back
-#define all(s) s.begin(),s.end()
-#define f(i,a,b) for(int i=a;i<b;i++)
-#define F(i,a,b) for(int i=a;i>=b;i--)
-#define PI 3.1415926535897932384626433832795
-#define BIG_INF 7000000000000000000LL
-#define mp make_pair
-#define eps 1e-9
-#define si(n) scanf("%d",&n)
-#define sll(n) scanf("%lld",&n)
-#define mod 1000000007
-#define mm 10000000
-#define INF (1<<29)
-#define SET(a) memset(a,-1,sizeof(a))
-#define CLR(a) memset(a,0,sizeof(a))
-#define FILL(a,v) memset(a,v,sizeof(a))
-#define EPS 1e-9
-#define min3(a,b,c) min(a,min(b,c))
-#define max3(a,b,c) max(a,max(b,c))
-#define READ freopen("input.txt", "r", stdin)
-#define WRITE freopen("output.txt", "w", stdout)
 
+map<char,int> memo;
 
-typedef long long int LL;
-
-string inttostring(int n)
-{
-  stringstream a;
-  a<<n;
-  string A;
-  a>>A;
-  return A;
-}
-
-int stringtoint(string A)
-{
-  stringstream a;
-  a<<A;
-  int p;
-  a>>p;
-  return p;
-}
-
-inline void inp(int &n ) {//fast input function
-    n=0;
-    int ch=getchar(),sign=1;
-    while( ch < '0' || ch > '9' ){if(ch=='-')sign=-1; ch=getchar();}
-    while( ch >= '0' && ch <= '9' )
-        n=(n<<3)+(n<<1)+ ch-'0', ch=getchar();
-    n=n*sign;
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-int main () {
-
-  int t;
-  inp(t);
-  char arr[63];
-  int val[37];
-  memset(val,-1,sizeof(val));
-  int c = 1;
-  while (t--) {
-    scanf("%s",arr);
-    int len = strlen(arr);
-    bool used = false;
-    
-    if (arr[0] >= 97 && val[arr[0]-'a'+10] == -1) {
-      val[arr[0]-'a'+10] = 1;
-    }
-    else if (arr[0] >= 48 && val[arr[0]-'0'] == -1) {
-      // printf("HERE\n");
-      val[arr[0]-'0'] = 1;
-    }
-
-    if (arr[1] >= 97 && val[arr[1]-'a'+10] == -1) {
-      val[arr[1]-'a'+10] = 0;
-      used = true;
-    }
-    else if (arr[1] >= 48 && val[arr[1]-'0'] == -1) {
-      val[arr[1]-'0'] = 0;
-      used = true;
-    }
-
-    
-    int j = 2;
-    for (int i = 2; i < len; i++) {
-      if (arr[i] >= 97 && val[arr[i]-'a'+10] == -1) {
-        if (used) {
-          val[arr[i]-'a'+10] = j;
-          j++;
-        }
-        else {
-          val[arr[i]-'a'+10] = 0;
-          used = true;
-        }
+int main() {
+   int nT; cin >> nT;
+   string str;
+   int k = 1;
+   while (nT-- && cin >> str) {
+      memo.clear();
+      int next = 0;
+      memo[str[0]] = 1;
+      for (int i = 1; i < str.size(); i++) {
+         if (memo.count(str[i]) == 0) {
+            memo[str[i]] = next;
+            next++;
+            if (next == 1) next++;
+         }
       }
-      else if (arr[i] >= 48 && val[arr[i]-'0'] == -1) {
-        if (used) {
-          val[arr[i]-'0'] = j;
-          j++;
-        }
-        else {
-          val[arr[i]-'0'] = 0;
-          used = true;
-        }
+      long long val = 0;
+      int base = next == 0 ? 2 : next;
+      for (int i = 0; i < str.size(); i++) {
+         val = val * base + memo[str[i]];
       }
-    }
-
-    // for (int i = 0; i < len; i++) {
-    //   if (arr[i] >= 97) {
-    //     // ans += val[arr[i]-'a'+10]*pow(len,len-i-1);
-    //     printf("%d ",val[arr[i]-'a'+10]);
-    //   }
-    //   else if (arr[i] >= 48) {
-    //     // ans += val[arr[i]-'0']*pow(len,len-i-1);
-    //     printf("%d ",val[arr[i]-'0']);
-    //   }
-    // }
-    // printf("\n%d\n", j);
-    // exit(0);
-
-
-    long long int ans = 0;
-    for (int i = 0; i < len; i++) {
-      if (arr[i] >= 97) {
-        // ans += (LL)(((LL)val[arr[i]-'a'+10])*((LL)pow(j,len-i-1)));
-        ans = ans * j + val[arr[i]-'a'+10];
-      }
-      else if (arr[i] >= 48) {
-        // ans += (LL)(((LL)val[arr[i]-'0'])*((LL)pow(j,len-i-1)));
-        ans = ans * j + val[arr[i]-'0'];
-      }
-    }
-    printf("Case #%d: %lli\n",c,ans);
-    c++;
-  }
+      cout << "Case #" << k << ": " << val << "\n";
+      k++;
+   }
+   return 0;
 }
