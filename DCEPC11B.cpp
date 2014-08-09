576435561ad89952864297a34e2cbe86873e00c4
Anmol <garganmol1993@gmail.com>
2014-08-10 04:29:12 +0530

    Updating code

---
+++ DCEPC11B.cpp
@@ -0,0 +1,98 @@
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <sstream>
+#include <algorithm>
+#include <cstdlib>
+#include <cstring>
+#include <map>
+#include <set>
+#include <queue>
+#include <cctype>
+#include <list>
+#include <stack>
+#include <fstream>
+#include <utility>
+#include <iomanip>
+
+using namespace std;
+#define pb push_back
+#define all(s) s.begin(),s.end()
+#define f(i,a,b) for(int i=a;i<b;i++)
+#define F(i,a,b) for(int i=a;i>=b;i--)
+#define PI 3.1415926535897932384626433832795
+#define BIG_INF 7000000000000000000LL
+#define mp make_pair
+#define eps 1e-9
+#define si(n) scanf("%d",&n)
+#define sll(n) scanf("%lld",&n)
+#define mod 1000000007
+#define mm 10000000
+#define INF (1<<29)
+#define SET(a) memset(a,-1,sizeof(a))
+#define CLR(a) memset(a,0,sizeof(a))
+#define FILL(a,v) memset(a,v,sizeof(a))
+#define EPS 1e-9
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+#define READ freopen("input.txt", "r", stdin)
+#define WRITE freopen("output.txt", "w", stdout)
+#define i64 unsigned long long
+
+
+typedef long long LL;
+
+string inttostring(int n)
+{
+  stringstream a;
+  a<<n;
+  string A;
+  a>>A;
+  return A;
+}
+
+int stringtoint(string A)
+{
+  stringstream a;
+  a<<A;
+  int p;
+  a>>p;
+  return p;
+}
+
+inline void inp(int &n ) {//fast input function
+    n=0;
+    int ch=getchar(),sign=1;
+    while( ch < '0' || ch > '9' ){if(ch=='-')sign=-1; ch=getchar();}
+    while( ch >= '0' && ch <= '9' )
+        n=(n<<3)+(n<<1)+ ch-'0', ch=getchar();
+    n=n*sign;
+}
+
+long long factMOD(int n, int MOD)
+{
+    long long res = 1; 
+    while (n > 0)
+    {
+        for (int i=2, m=n%MOD; i<=m; i++)
+            res = (res * i) % MOD;
+        if ((n/=MOD)%2 > 0) 
+            res = MOD - res;
+    }
+    return res;
+}
+
+
+int main () {
+  int t;
+  inp(t);
+
+  while (t--) {
+    int n, p;
+    inp(n); inp(p);
+    printf("%lli\n",factMOD(n,p));
+  }
+}
